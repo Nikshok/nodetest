ced00b2b184b55117dca29684b7a38fcc896f168
SMalashta <Sergey.Malashta@noveogroup.com>
2017-03-09 16:07:47 +0700

    fix code style

--- main.js
+++ main.js
@@ -4,5 +4,19 @@
 (function() {
     'use strict';
 
-    
+    var xhr = new XMLHttpRequest(),
+        method = "GET",
+        url = "http://localhost:3001/users";
+
+
+    xhr.open(method, url, true);
+
+    xhr.onreadystatechange = function () {
+        if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            console.log(xhr.responseText);
+        }
+    };
+
+    xhr.send();
+
 })();
